22038edc4a231be54e04d6e24fa66d9b3f5bf391
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-06 21:50:54 -0600

    Hard Mode Complete

--- scripts/client.js
+++ scripts/client.js
@@ -7,10 +7,105 @@ phirePhiterApp.controller('PhirePhiterController', function(){
 
   self.people = [
     {
-      "firstName": "Luke",
-      "lastName": "Schlangen",
-      "git_username": "LukeSchlangen",
-      "shoutout": "Thanks for being awesome during the angular lecture today."
+      "firstName" : "Melissa E",
+      "lastName" :"Hare",
+      "git_username" : "Melissa-Elizabeth",
+      "shoutout" : ""
+    },
+    {
+      "firstName" : "Chad",
+      "lastName" :"Sundberg",
+      "git_username" : "chadsundberg",
+      "shoutout" : "Celina!!  She taught me how to use two fingers to scroll in my open windows!  Oh, and I'd like to thank var taco!"
+    },
+    {
+      "firstName" :"Christine",
+      "lastName" :"Pogatchnik",
+      "git_username" : "kikipogo",
+      "shoutout" : "Everyone"
+    },
+    {
+      "firstName": "Thomas",
+      "lastName" :"Johander",
+      "git_username" : "lopan455",
+      "shoutout" : "Yo! Thanks for that!"
+    },
+    {
+      "firstName": "Edmund",
+      "lastName" :"Wippler",
+      "git_username" : "edwippler",
+      "shoutout" : ""
+    },
+    {
+      "firstName": "Jonny",
+      "lastName" :"Holupchinski",
+      "git_username" : "jonholup",
+      "shoutout" : ""
+    },
+    {
+      "firstName": "Catherine",
+      "lastName" :"Jarocki",
+      "git_username" : "cjaro",
+      "shoutout" : "Thank you to the internet of resources!"
+    },
+    {
+      "firstName": "Kris",
+      "lastName" :"Jensen",
+      "git_username" : "kristinejensen",
+      "shoutout" : "Thank you to Luke, Kris, and Chris for being great instructors!"
+    },
+    {
+      "firstName": "Paul",
+      "lastName" :"Dunkirk",
+      "git_username" : "pauldunkirk",
+      "shoutout" : "Phi peops rock!"
+    },
+    {
+      "firstName": "Celina",
+      "git_username" : "celina",
+      "shoutout" : "Peace out!"
+    },
+    {
+      "firstName": "Chris",
+      "lastName" :"Stanton",
+      "git_username" : "chris-stanton",
+      "shoutout" : ""
+    },
+    {
+      "firstName" : "Philip",
+      "lastName" : "Curtis",
+      "git_username" : "PCSailor",
+      "shoutout" : "Thanks to Luke and Kris, and Chris, Scott, & Fred"
+    },
+    {
+      "firstName" : "Sam",
+      "lastName" : "Velie",
+      "git_username" : "samvelie",
+      "shoutout" : "Love y'all "
+    },
+    {
+      "firstName" : "Paige",
+      "lastName" : "Bystrom",
+      "git_username" : "Radmint",
+      "shoutout" : "Thanks to Phi cohort for being awesome, positive people! "
+    },
+    {
+      "firstName" : "Jeremy",
+      "lastName" : "Leif",
+      "git_username" : "JL-golf14",
+      "shoutout" : "break down the Phirewalls of doubt!!!"
+    },
+    {
+      "firstName" : "Kris",
+      "lastName" : "Szafranski",
+      "git_username" : "kdszafranski",
+      "shoutout" : "Thank you to Phi for your open, warm, and thoughtful speeches. You rekindle my faith in humanity!"
+    },
+    {
+      "firstName" : "Luke",
+      "lastName" : "Schlangen",
+      "git_username" : "LukeSchlangen",
+      "shoutout" : "Thanks for being awesome during the angular lecture today."
     }
   ];
 
